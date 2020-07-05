4a1277f8790e0586efb468c52cca580678570d83
Ryan Ameri <ryan@internettradie.com.au>
2020-07-05 19:39:07 +1000

    changed panagram to use regex

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,24 +1,9 @@
 export const isPangram = (inputString) => {
-   const alphabet = new Map();
-   
-   //Initialize map to 0
-  for (let i = 97; i<= 122; i++) {
-    alphabet.set(String.fromCharCode(i), 0);
-  }
 
-  //increment the map value for every letter in the inputString
-  [...inputString.toLowerCase()].forEach((letter) => {
-    if (alphabet.has(letter))
-        alphabet.set(letter, alphabet.get(letter) +1)
-      
-  });
+  //I did not come up with the regex myself, borrowed from https://stackoverflow.com/questions/32557299/javascript-pangram-regex
 
-  //If any value in Map is 0, return false, else return true
-  for (let value of alphabet.values()) {
-    if (value === 0) {
-      return false;
-    }
-  }
-  return true;
+  //I think I can understand what it's doing, but I could not write this myself
+  const regex = /([a-z])(?!.*\1)/g;
+  return (inputString.toLowerCase().match(regex) || []).length === 26;
 };
 
