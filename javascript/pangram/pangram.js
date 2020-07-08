e45d523919367ab592a8be1327dd2f4d90326ac6
Ryan Ameri <ryan@internettradie.com.au>
2020-07-08 23:14:33 +1000

    changed pangram based on mentor feedback. Uses array.prototype.every() and String.prototopye.includes() for a fast and easy to read implementation

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,9 +1,11 @@
 export const isPangram = (inputString) => {
-
-  //I did not come up with the regex myself, borrowed from https://stackoverflow.com/questions/32557299/javascript-pangram-regex
-
-  //I think I can understand what it's doing, but I could not write this myself
-  const regex = /([a-z])(?!.*\1)/g;
-  return (inputString.toLowerCase().match(regex) || []).length === 26;
+  const alphabet = [];
+  
+  //Initialize array to alphabet
+  for (let i = 0; i<= 25; i++) {
+    alphabet[i]  = String.fromCharCode(i + 97);
+  }
+  return alphabet
+          .every(letter => inputString.toLowerCase().includes(letter))
 };
 
