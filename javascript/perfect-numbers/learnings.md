635248294f8aa20425049761e7bf466418d22ba5
Ryan Ameri <ryan@internettradie.com.au>
2020-07-09 17:35:20 +1000

    performance improvement by getting rid of the factors array. No need to reduce anymore

--- javascript/perfect-numbers/learnings.md
+++ javascript/perfect-numbers/learnings.md
@@ -1,1 +1,3 @@
 # Perfect Numbers Learnings
+## Futher Improvements
+* This doesn't use any array methods. But it's really clean and simple and performant with just one loop. I don't see the point of overcomplicating it. 
--- javascript/perfect-numbers/perfect-numbers.js
+++ javascript/perfect-numbers/perfect-numbers.js
@@ -1,14 +1,12 @@
 export const classify = (inputNum) => {
   if (inputNum < 1)
     throw new Error("Classification is only possible for natural numbers.");
-  const factors = [];
+  let aliquot = 0;
   for (let i = 1; i < inputNum; i++) {
     if (inputNum % i === 0) {
-      factors.push(i)
+      aliquot += i;
     }
   }
-  const aliquot = factors.reduce((a, b) => a + b, 0);
-  console.log(aliquot);
   return aliquot === inputNum ? "perfect" 
     : aliquot > inputNum ? "abundant" : "deficient";
 };
