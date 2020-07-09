c3aca376f1e17bbd48fbe272116df0646233e883
Ryan Ameri <ryan@internettradie.com.au>
2020-07-09 16:10:36 +1000

    implemented prime factors. Rather crude and procedural implementation but it does the job well enough

--- javascript/prime-factors/prime-factors.js
+++ javascript/prime-factors/prime-factors.js
@@ -1,8 +1,15 @@
-//
-// This is only a SKELETON file for the 'Prime Factors' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const primeFactors = () => {
-  throw new Error("Remove this statement and implement this function");
+export const primeFactors = (inputNum) => {
+  let num = inputNum;
+  let factors = [];
+  while (num > 1){
+    for (let i = 2; i <= num; i++){
+      if (num % i === 0){
+        factors.push(i);
+        num /= i;
+        break;
+      }
+    }
+  }
+  return factors;
+  
 };
