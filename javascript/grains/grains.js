bb581bbb82d40648cf36c933c951ee25d3e1d85f
Ryan Ameri <ryan@internettradie.com.au>
2020-07-09 16:25:45 +1000

    initial commit of grains

---
+++ javascript/grains/grains.js
@@ -0,0 +1,11 @@
+//
+// This is only a SKELETON file for the 'Grains' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+export const square = () => {
+  throw new Error("Remove this statement and implement this function");
+};
+
+export const total = () => {
+  throw new Error("Remove this statement and implement this function");
+};
