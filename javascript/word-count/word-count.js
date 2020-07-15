daa626b1e1a2451719d0bbf3e4f4111407521d7d
Ryan Ameri <ryan@internettradie.com.au>
2020-07-15 18:53:13 +1000

    initial commit of pythagorean triplet

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,16 +1,18 @@
-export const countWords = (inputString) => {
-
+const countWords = (inputString) => {
   const wordCount = {};
-  
+
   const cleanInput = inputString
     .toString()
     .trim()
     .toLowerCase()
     .match(/([A-Za-z0-9]+('[a-z])?)/g);
-  
-  cleanInput.forEach(word => {
-    wordCount.hasOwnProperty(word) ? 
-      wordCount[word] = wordCount[word] +1 : wordCount[word] = 1;
+
+  cleanInput.forEach((word) => {
+    wordCount.hasOwnProperty(word)
+      ? (wordCount[word] = wordCount[word] + 1)
+      : (wordCount[word] = 1);
   });
   return wordCount;
 };
+
+console.log(countWords("Jello mother sodhi"));
