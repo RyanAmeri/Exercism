02aefaa9f7284bdbc35a79910a3955baaf618f65
Ryan Ameri <ryan@internettradie.com.au>
2020-07-30 23:51:07 +1000

    implemented isogram exercise

--- javascript/isogram/isogram.js
+++ javascript/isogram/isogram.js
@@ -1,8 +1,10 @@
-//
-// This is only a SKELETON file for the 'Isogram' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const isIsogram = () => {
-  throw new Error("Remove this statement and implement this function");
+export const isIsogram = (input) => {
+  const charset = new Set();
+  return [...input].every((letter) => {
+    letter = letter.toLowerCase();
+    if (letter === " " || letter === "-") return true;
+    if (charset.has(letter)) return false;
+    else charset.add(letter);
+    return true;
+  });
 };
---
+++ javascript/isogram/learnings.md
@@ -0,0 +1,8 @@
+# Isogram Exercise Learnings
+
+## Revision of topics
+
+- Spreading a string into an array
+- Using Array.prototype.every
+- Using a Set
+- Proud I solved this without using regexp or any other charCode hack. Using Set and every() is a lot more elegant and readable
