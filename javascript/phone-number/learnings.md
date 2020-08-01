cb8d94cc20b158c7671f96b238c2b858f895c001
Ryan Ameri <ryan@internettradie.com.au>
2020-08-01 13:50:23 +1000

    implemented phone number exercise

--- javascript/phone-number/learnings.md
+++ javascript/phone-number/learnings.md
@@ -1,1 +1,8 @@
 # Phone Number Exercise Learnings
+
+- String manipulation using regexp
+- String.prototype.slice() method
+- String.prototype.startsWith() method
+- String.prototype.replace() method
+- String.prototype.replace() method
+- RegExp.prototype.test() method
--- javascript/phone-number/phone-number.js
+++ javascript/phone-number/phone-number.js
@@ -1,8 +1,29 @@
-//
-// This is only a SKELETON file for the 'Phone Number' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const clean = (input) => {
+  const regex = /\D+/gi;
+  const wordRegEx = /[a-z]+/gi;
+  const puncRegEx = /@|:|!/g;
+  //Input checking
+  if (puncRegEx.test(input)) throw new Error("Punctuations not permitted");
+  if (wordRegEx.test(input)) throw new Error("Letters not permitted");
 
-export const clean = () => {
-  throw new Error("Remove this statement and implement this function");
+  //Clean up input
+  let clean = input.replace(regex, "");
+  if (clean.length < 10) throw new Error("Incorrect number of digits");
+  if (clean.length > 11) throw new Error("More than 11 digits");
+  //If number is 11 digits, it's only acceptable if the first digit is 1
+  if (clean.length === 11) {
+    if (clean.startsWith("1")) clean = clean.slice(1);
+    else throw new Error("11 digits must start with 1");
+  }
+  //Area code and exchange checking
+  if (clean.slice(0, 1) === "0")
+    throw new Error("Area code cannot start with zero");
+  if (clean.slice(0, 1) === "1")
+    throw new Error("Area code cannot start with one");
+  if (clean.slice(3, 4) === "0")
+    throw new Error("Exchange code cannot start with zero");
+  if (clean.slice(3, 4) === "1")
+    throw new Error("Exchange code cannot start with one");
+
+  return clean;
 };
