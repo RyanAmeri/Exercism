e1fe98fb42339a519e872276c037c93264d38b19
Ryan Ameri <ryan@internettradie.com.au>
2020-08-01 23:52:03 +1000

    implemented anagram exercise

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -1,8 +1,29 @@
-//
-// This is only a SKELETON file for the 'Anagram' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const findAnagrams = (word, list) => {
+  return list.filter((item) => {
+    return [...item].every((char) => {
+      return (
+        [...word.toLowerCase()].includes(char.toLowerCase()) &&
+        word.length === item.length &&
+        word.toLowerCase() !== item.toLowerCase() &&
+        compareMaps(countChars(word), countChars(item))
+      );
+    });
+  });
+};
 
-export const findAnagrams = () => {
-  throw new Error("Remove this statement and implement this function");
+function countChars(word) {
+  const map = new Map();
+  const newWord = word.toLowerCase();
+  [...newWord].forEach((char) => {
+    map.has(char) ? map.set(char, map.get(char) + 1) : map.set(char, 1);
+  });
+  return map;
+}
+
+function compareMaps(map1, map2) {
+  if (map1.size !== map2.size) return false;
+  for (let [key] of map1) {
+    if (map1.get(key) !== map2.get(key)) return false;
+  }
+  return true;
 }
---
+++ javascript/anagram/learnings.md
@@ -0,0 +1,7 @@
+# Anagram Exercise Learnings
+
+## Revisions
+
+- Using Array.prototype.every()
+- Using Array.prototype.filter()
+- Counting the number of occurences of every character in a word and setting them in a map
